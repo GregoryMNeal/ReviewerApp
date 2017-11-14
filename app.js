275d4fedf246f72a569ebbeee3d2ac2727c0a2b7
Gregory Neal <gnealsr@gmail.com>
2017-11-14 10:02:54 -0600

    Added User Login

--- app.js
+++ app.js
@@ -1,21 +1,71 @@
 // Restaurant Review App
 
+// Imports
 var express = require('express'); // used to make an Express app
 var app = express(); // make the app
-app.set('view engine', 'hbs'); // use handlebars for template rendering
-app.use(express.static('public')); // Setup express to serve the files in the public folder
+var session = require('express-session'); // used for user login
 var pgp = require('pg-promise')({
   // initialization options
-}); // for accessing database
-var db = pgp({database: 'restaurant'});
-const body_parser = require('body-parser');
+}); // for accessing the database
+var db = pgp({database: 'restaurant'}); // also for accessing the database
+var body_parser = require('body-parser'); // used to retrieve input from HTML forms
+
+// Application setup
+app.set('view engine', 'hbs'); // use handlebars for template rendering
+app.use(express.static('public')); // Setup express to serve the files in the public folder
 app.use(body_parser.urlencoded({extended: false}));
+
+// saves a session
+app.use(session({
+  secret: process.env.SECRET_KEY || 'dev',
+  resave: true,
+  saveUninitialized: false,
+  cookie: {maxAge: 60000}
+}));
+
+// route the User to the login page if they are not logged in
+app.use(function (req, resp, next) {
+  if (req.session.user) {
+    next(); // already logged in
+  } else if (req.path == '/login') {
+    next(); // already navigating to the login page
+  } else {
+    resp.redirect('/login'); // route to thelogin page
+  }
+});
 
 // get method for root URL:/
 app.get('/', function (req, resp, next) {
   var context = {title: 'Restaurant Review'};
   resp.render('index.hbs', context);
 });
+
+
+// get method for login
+app.get('/login', function (req, resp, next) {
+  var context = {title: 'Login'};
+  resp.render('login.hbs', context);
+});
+
+// post method for login
+app.post('/login', function (req, resp, next) {
+  var username = req.body.username; // get user name from the form
+  var password = req.body.password; // get password from the form
+  var q = 'SELECT * from reviewer WHERE email = $1';
+  db.one(q, username)
+      .then(function (result) {
+      if (result.pword == password) {
+        req.session.user = username; // set up a user session
+        resp.redirect('/')
+      } else {
+        resp.send("Invalid Credentials");
+      }
+    })
+    .catch(function (error) {
+      resp.send("Invalid Credentials");
+    });
+});
+
 
 // Display list of restaurants that match search criteria
 app.get('/search', function (req, resp, next) {
@@ -29,6 +79,7 @@ app.get('/search', function (req, resp, next) {
     })
     .catch(next);
 });
+
 
 // get method for adding a restaurant
 app.get('/restaurant/new', function (req, resp, next) {
@@ -57,6 +108,7 @@ app.post('/restaurant/submit_new', function (req, resp, next) {
       .catch(next);
 });
 
+
 // Display details for a restaurant
 app.get('/restaurant/:id', function (req, resp, next) {
   var id = req.params.id;
@@ -70,6 +122,7 @@ app.get('/restaurant/:id', function (req, resp, next) {
     })
     .catch(next);
 });
+
 
 // get method for adding a review
 app.get('/addreview', function (req, resp, next) {
@@ -100,6 +153,7 @@ app.post('/addreview', function (req, resp, next) {
       })
       .catch(next);
 });
+
 
 // Listen for requests
 app.listen(8000, function() {
